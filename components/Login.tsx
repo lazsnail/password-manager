50ee10b97ef958d156bb089f936d5db02ff5464e
lazsnail <larrybain01@gmail.com>
2023-07-22 20:48:40 +1000

    center text on email request

--- components/Login.tsx
+++ components/Login.tsx
@@ -65,7 +65,7 @@ export default function Login() {
   return (
     <>
       {signedUp ? (
-        <div>
+        <div className="text-center">
           Please verify the email sent to <b>{email}</b>
         </div>
       ) : displayType === "signIn" ? (
